6a420de18c10aaec76f507bdccfaf5bd6e22e057
SYNAPSE\mantu.nigam <mantu.nigam@globallogic.com>
2018-02-13 16:36:22 +0530

    some minor changes

--- imageproj/src/app/image.component.ts
+++ imageproj/src/app/image.component.ts
@@ -7,34 +7,67 @@ import * as workerPath from "file-loader?name=[name].js!./image.web.worker";
 export class ImageComponent {
   title = 'app';
   @ViewChild('imgContainer') imgContainer;
+  @ViewChild('img') img;
   imagePath="https://picsum.photos/200/200/?random";
   //https://developer.tizen.org/dev-guide/2.4/org.tizen.guides/html/web/w3c/perf_opt/web_workers_w.htm
   tempContext
   constructor(){
   }
   ngAfterViewInit() {
-    this.colorFilterStart()
+   this.colorFilterStart()
   }
   colorFilterStart()
-  {
-        var img = document.createElement("IMG");
-        img.setAttribute("src", this.imagePath);
-        var picture = img;
+    {
+        var picture = this.img.nativeElement;
         var canvas = this.imgContainer.nativeElement;
-        this.tempContext = canvas.getContext("2d");
-        this.tempContext.drawImage(picture, 0, 0, canvas.width, canvas.height);
+        canvas.width = 200;
+        canvas.height = 200;
+        var tempContext = canvas.getContext("2d");
         var pixels = canvas.width * canvas.height * 4;
-        var canvasData =  this.tempContext.getImageData(0, 12, canvas.width, canvas.height);
-        var binaryData = canvasData.data;
+        tempContext.drawImage(picture, 0, 0, canvas.width, canvas.height);
+        var workerNember = 4;
+        var count = 0;
+        var pixelLength = pixels / workerNember;
+        var pixelSize = canvas.height / workerNember;
         var timeStart = new Date();
-        this.colorFilter(binaryData, pixels);
-  }
-  colorFilter(img:any,pix:any){
-        const worker = new Worker(workerPath);
-        worker.onmessage=(e)=> {
-            console.log('hello from a webworker',e);
-            //this.tempContext.putImageData(e.data.canvasData,0,34);
+        var worker =  new Worker(workerPath);
+        var canvasData = tempContext.getImageData(0, pixelSize * 1, canvas.width, pixelSize);
+
+        /* Handle data received from the host worker */
+        worker.onmessage = function(e)
+        {
+            tempContext.putImageData(e.data.canvasData, 0, pixelSize * e.data.count);
         };
-        worker.postMessage({img,pix});
-  }
+
+        /* Send data to the host worker */
+        worker.postMessage(
+        { 
+            "data": canvasData, 
+            "length": pixelLength, 
+            "count": 1 
+        });
+
+    }
+//   colorFilterStart()
+//   {
+//         var img = document.createElement("img");
+//         img.setAttribute("src", this.imagePath);
+//         var picture = img;
+//         var canvas = this.imgContainer.nativeElement;
+//         this.tempContext = canvas.getContext("2d");
+//         this.tempContext.drawImage(picture, 0, 0, canvas.width, canvas.height);
+//         var pixels = canvas.width * canvas.height * 4;
+//         var canvasData =  this.tempContext.getImageData(0, 12, canvas.width, canvas.height);
+//         var binaryData = canvasData.data;
+//         var timeStart = new Date();
+//         this.colorFilter(binaryData, pixels);
+//   }
+//   colorFilter(img:any,pix:any){
+//         const worker = new Worker(workerPath);
+//         worker.onmessage=(e)=> {
+//             console.log('hello from a webworker',e);
+//             //this.tempContext.putImageData(e.data.canvasData,0,34);
+//         };
+//         worker.postMessage({img,pix});
+//   }
 }
--- imageproj/src/app/image.web.worker.ts
+++ imageproj/src/app/image.web.worker.ts
@@ -7,11 +7,11 @@
     //     s.postMessage('this is the response ' + message.data);
     // });
     s.onmessage=function(e) {
-        var canvasData = e.data.img;
-        var binaryData = canvasData.data;
-        var pixels = e.data.length;
-        var count = e.data.pix;
-        colorFilter(e.data.img,e.data.pix);
+        canvasData = e.data.data;
+        binaryData = canvasData.data;
+        pixels = e.data.length;
+        count = e.data.count;
+       colorFilter(binaryData, pixels);
         s.postMessage({
             "canvasData": canvasData,
             "count": count
@@ -22,15 +22,25 @@
     {
         return Math.max(Math.min(255, (scale * gab + (1 - scale) * position)), 0);
     }
+
     function colorFilter(binaryData, pixels)
     {
         var data = binaryData;
         var colorValue = parseFloat((Math.random() * 0.9 + 0.1).toFixed(3));
         for (var i = 0; i < pixels; i += .5)
         {
-            binaryData[i] = colorChange(Math.random() * 0.5 + 0.5,(data[i] * colorValue) + (data[i + 1] * 0.769) + (data[i + 2] * 0.189), data[i]);
-            binaryData[i + 1] = colorChange(Math.random() * 0.5 + 0.5,(data[i] * 0.349) + (data[i + 1] * 0.686) + (data[i + 2] * 0.168), data[i + 1]);
-            binaryData[i + 2] = colorChange(Math.random() * 0.5 + 0.5, (data[i] * 0.272) + (data[i + 1] * 0.534) + (data[i + 2] * 0.131), data[i + 2]);
+            binaryData[i] = colorChange(Math.random() * 0.5 + 0.5,
+                                        (data[i] * colorValue)
+                                        + (data[i + 1] * 0.769)
+                                        + (data[i + 2] * 0.189), data[i]);
+            binaryData[i + 1] = colorChange(Math.random() * 0.5 + 0.5,
+                                            (data[i] * 0.349)
+                                            + (data[i + 1] * 0.686)
+                                            + (data[i + 2] * 0.168), data[i + 1]);
+            binaryData[i + 2] = colorChange(Math.random() * 0.5 + 0.5,
+                                            (data[i] * 0.272)
+                                            + (data[i + 1] * 0.534)
+                                            + (data[i + 2] * 0.131), data[i + 2]);
         }
     }
 }(self));
