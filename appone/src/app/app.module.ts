49f1c0b6b121e82af9b477ac7e3eef4a00d61dff
SYNAPSE\mantu.nigam <mantu.nigam@globallogic.com>
2018-02-14 16:40:07 +0530

    router module integrated

--- appone/src/app/app.module.ts
+++ appone/src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NplmoduleModule } from './nplmodule/nplmodule.module';
 import { NaturalLanguageService } from './natural.language.service';
-
+const appRoutes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +28,8 @@ import { NaturalLanguageService } from './natural.language.service';
   imports: [
     FormsModule,
     BrowserModule,
-    NplmoduleModule
+    NplmoduleModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [NaturalLanguageService],
   bootstrap: [AppComponent]
